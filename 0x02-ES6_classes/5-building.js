4e2ee5f00434b2a30b8d5c800d0fb7f471f58d3f
kan-215 <katamialvin369@gmail.com>
2024-10-03 23:47:38 +0300

    alx-backend-javascript

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,9 +1,26 @@
+// 5-building.js
 export default class Building {
   constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Square footage must be a number');
+    }
+
     this._sqft = sqft;
+
+    // Prevent creating an instance of an abstract class
+    if (new.target === Building) {
+      throw new Error('Cannot instantiate an abstract class');
+    }
   }
-  get sqft() {return this._sqft}
+
+  // Getter for sqft
+  get sqft() {
+    return this._sqft;
+  }
+
+  // Abstract method that must be implemented by subclasses
   evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage')
+    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
+

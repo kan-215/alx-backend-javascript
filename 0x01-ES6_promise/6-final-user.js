acb3cb222dc470b0bb290f5420f79df780e5ad82
kan-215 <katamialvin369@gmail.com>
2024-10-05 00:07:58 +0300

    0x00-ES6_promise

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,10 +1,13 @@
-import signUpUser from './4-user-promise'
-import uploadPhoto from './5-photo-reject'
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignUp(firstName, lastName, fileName) {
-  Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)]).then((values) => {
-    return [{status: values[1].status, value: values[0]}];
-  }).catch(values => {
-    return [{status: values[1].status, value: values[1]}];
-  });
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
 }

a61275961b5d75e92978b464165ff99017d7d0fb
kan-215 <katamialvin369@gmail.com>
2024-10-05 00:23:43 +0300

    0x00-ES6_basic

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,28 +1,9 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = createIteratorObject;
-function createIteratorObject(report) {
-  const departments = Object.values(report.allEmployees);
-  let currentIndex = 0;
-  let employeeIndex = 0;
-  return {
-    next() {
-      const employees = departments[currentIndex];
-      const done = currentIndex >= departments.length && employeeIndex >= employees.length;
-      if (employeeIndex >= employees.length) {
-        currentIndex++;
-        employeeIndex = 0;
+export default function createIteratorObject(report) {
+  return (function* _() {
+    for (const department of Object.values(report.allEmployees)) {
+      for (const employee of department) {
+        yield employee;
       }
-      return {
-        value: done ? undefined : employees[employeeIndex++],
-        done: done
-      };
-    },
-    [Symbol.iterator]() {
-      return this;
     }
-  };
+  }());
 }

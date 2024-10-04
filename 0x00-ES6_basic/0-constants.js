a9972fe41c0e5b0ec19386683220688200e2afdd
kan-215 <katamialvin369@gmail.com>
2024-10-05 00:13:54 +0300

    0x00-ES6_basic

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,14 +1,15 @@
-function taskFirst() {
+export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
 }
 
-function getLast() {
+export function getLast() {
   return ' is okay';
 }
 
-function taskNext() {
+export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
+
   return combination;
 }

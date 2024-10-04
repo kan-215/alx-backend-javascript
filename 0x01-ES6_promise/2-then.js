6e7c9051cd2c732babf2497bf1809f530ccf79da
kan-215 <katamialvin369@gmail.com>
2024-10-05 00:03:11 +0300

    0x00-ES6_promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,10 @@
-export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
-    console.log('Got a response from the API');
-    return { status: 200, body: 'success' };
-  })
-    .catch(() => new Error());
-  /* if (promise.then()){
-    console.log('Got a response from the API')
-    return {status: 200, body: 'success'}
-  } else {
-    return {}
-  } */
+function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => Error())
+    .finally(() => console.log('Got a response from the API'));
 }
+export default handleResponseFromAPI;

f12ad41461ba6ce030e1473f052c51a02554d6fc
kan-215 <katamialvin369@gmail.com>
2024-10-05 00:48:30 +0300

    0x00-ES6_classes

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,9 +1,25 @@
-export default class Building {
+class Building {
   constructor(sqft) {
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
+    }
+    this.sqft = sqft;
+  }
+
+  set sqft(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft nust be a number');
+    }
     this._sqft = sqft;
   }
-  get sqft() {return this._sqft}
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage')
+
+  get sqft() {
+    return this._sqft;
   }
 }
+
+export default Building;

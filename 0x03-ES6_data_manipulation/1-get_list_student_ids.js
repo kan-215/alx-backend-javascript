fec7fa8e05132b567b34a092ecb0aa5a7a47a64c
kan-215 <katamialvin369@gmail.com>
2024-10-08 22:04:34 +0300

    1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,6 @@
-export default function getListStudentIds(arr) {
-    if (!Array.isArray(arr)){
-        return [];
-    }
-
-    return arr.map((student) => student.id)
+export default function getListStudentIds(students) {
+  if (students instanceof Array) {
+    return students.map((student) => student.id);
+  }
+  return [];
 }

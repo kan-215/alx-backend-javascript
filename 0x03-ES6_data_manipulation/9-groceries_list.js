6c2d02889327888e25534f0d63f826e2c3c7fb9d
kan-215 <katamialvin369@gmail.com>
2024-10-08 21:27:05 +0300

    9-groceries_list.js

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,13 +1,10 @@
-export const weakMap = new WeakMap();
-
-export const queryAPI = (endpoint) => {
-  if (weakMap.has(endpoint)) {
-    const endpointData = weakMap.get(endpoint);
-    if (endpointData >= 4) {
-      throw new Error('Endpoint load is high');
-    }
-    weakMap.set(endpoint, (endpointData + 1));
-  } else {
-    weakMap.set(endpoint, 1);
-  }
-};
+export default function groceriesList() {
+  const values = [
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ];
+  return new Map(values);
+}

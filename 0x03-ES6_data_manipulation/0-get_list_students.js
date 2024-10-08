06e09b7e3def3fc7e14f8a4948631919e559b405
kan-215 <katamialvin369@gmail.com>
2024-10-08 21:59:00 +0300

    0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,13 +1,7 @@
 export default function getListStudents() {
-    const id = [1, 2, 5];
-    const firstName = ["Guillaume", "James", "Serena"];
-    const location = ["San Francisco",  "Columbia"];
-
-    const client = [
-        {id: id[0], firstName:`${firstName[0]}`, location: `${location[0]}`},
-        {id: id[1], firstName:`${firstName[1]}`, location: `${location[1]}`},
-        {id: id[2], firstName:`${firstName[2]}`, location: `${location[0]}`}
-    ];
-
-    return client;
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
 }

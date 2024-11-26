941c216743c7fab20202c22386b48b6a05cd8c6a
kan-215 <katamialvin369@gmail.com>
2024-11-26 23:59:21 +0300

    basic node js

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -8,7 +8,6 @@ const DB_FILE = process.argv.length > 2 ? process.argv[2] : '';
 /**
  * Counts the students in csv databse file
  * @param {String} dataPath path to the CSV data file.
- * 
  * */
 const countStudents = (dataPath) => new Promise((resolve, reject) => {
   if (!dataPath) {

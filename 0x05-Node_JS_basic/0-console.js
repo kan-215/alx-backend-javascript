a879f0f51c4b734e34e0a28fb3b9ef59d7576a7c
kan-215 <katamialvin369@gmail.com>
2024-11-26 23:08:12 +0300

    o-console.js

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,9 +1,10 @@
 /**
  * prints the string argument in STDOUT
  * @param {String} message to display.
+ * @autho kan-215
  */
 
 function displayMessage(message) {
 	console.log(message);
-}
+};
 module.exports = displayMessage;
